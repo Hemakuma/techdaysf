b064ec7be9d5d8c10ea177851b2f816ebed96788
Hemant <hemakuma@cisco.com>
2017-04-24 09:30:47 -0700

    updated

---
+++ walmart/library/hk_create_tenantv4.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import *
+import socket
+import json
+import requests
+
+def main():
+
+  fields = {
+        "action": dict(choices=['post', 'get'], default='post'),
+        "tenant_name": dict(type='str'),
+        "creds":dict(),
+        # "host": dict(required=True),
+        # "username": dict(type='str', default='admin'),
+        # "password": dict(type='str', default='rovikumar'),
+        "protocol": dict(choices=['http', 'https'], default='http'),
+        "state": dict(choices=['present', 'absent'], default="present", type='str' )
+    }
+  module = AnsibleModule(argument_spec=fields)
+
+  tenant_name = module.params['tenant_name']
+  username = module.params['creds']['username']
+  password = module.params['creds']['password']
+  protocol = module.params['creds']
+  host = socket.gethostbyname(module.params['creds']['host'])
+  action = module.params['action']
+  state = module.params['state'].lower()
+#
+#   if state == "present":
+#     status_state = "created"
+#   if state == "absent":
+#     status_state = "deleted"
+#
+#   post_uri = '/api/mo/uni.json'
+#   get_uri = 'api/node/class/fvTenant.json'
+#
+#   ''' Config payload to enable the physical interface '''
+#
+#   config_data = {
+#       "fvTenant":{
+#           "attributes":{
+#               "name":tenant_name,
+#               "status":status_state
+#           }
+#       }
+#   }
+#
+#   payload_data = json.dumps(config_data)
+#
+#   ''' authentication || || Throw an error otherwise'''
+#   apic = "{0}://{1}/".format(protocol, host)
+#
+#   auth = dict(aaaUser=dict(attributes=dict(name=username, pwd=password)))
+#   url=apic+'api/aaaLogin.json'
+#
+#   authenticate = requests.post(url, data=json.dumps(auth), timeout=2, verify=False)
+#
+#   if authenticate.status_code != 200:
+#       module.fail_json(msg='could not authenticate to apic', status=authenticate.status_code, response=authenticate.text)
+#
+#
+#
+#   ''' Sending the request to APIC '''
+#   if post_uri.startswith('/'):
+#       post_uri = post_uri[1:]
+#   post_url = apic + post_uri
+#
+#   if get_uri.startswith('/'):
+#       get_uri = get_uri[1:]
+#   get_url = apic + get_uri
+#
+#   if action == 'post':
+#       req = requests.post(post_url, cookies=authenticate.cookies,
+#                           data=payload_data, verify=False)
+#   elif action == 'get':
+#       req = requests.get(get_url, cookies=authenticate.cookies,
+#                          data=payload_data, verify=False)
+#
+#   ''' Check response status and parse it for status || Throw an error otherwise '''
+#   response = req.text
+#   status = req.status_code
+#
+#   #load the error into a python dictionary
+#   response1 = json.loads(response)
+#   # use aci gui inspector to get this data
+#   #codenum = response1["imdata"][0]["error"]["attributes"]["code"]
+#   changed = False
+#
+# #when a object is creted, it returns a empty list eg imdata[] ..see the inspector
+# # to check to empty list , just not list ...imdata has list of stuff [] ..checkout when the object already exists
+#   if not response1["imdata"]:
+#       if action == 'post':
+#         changed = True
+#       else:
+#         changed = False
+#
+#   elif response1["imdata"][0]["error"]["attributes"]["code"] == '103':
+#       if action == 'post':
+#         changed = False
+#
+#   else:
+#     module.fail_json(msg='error issuing api request',
+#                      response=response, status=status)
+#
+#   results = {}
+#   results['status'] = status
+#   results['response'] = response
+#   results['changed'] = changed
+#
+#   module.exit_json(**results)
+
+#  module.exit_json(changed=False, meta=codenum)
+  module.exit_json(changed=False, meta=module.params))
+
+
+if __name__ == '__main__':
+    main()
